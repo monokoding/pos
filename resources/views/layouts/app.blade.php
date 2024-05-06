659e90eabfe65e8b311500f52613d7068e291b24
Jordan Aditya <jordanbrilliantaditya27@gmail.com>
2024-05-06 20:25:50 +0700

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,161 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+
+    <!-- General CSS Files -->
+    <link rel="stylesheet" href="{{ url('assets/modules/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ url('assets/modules/fontawesome/css/all.min.css') }}">
+
+    <!-- CSS Libraries -->
+    <link rel="stylesheet" href="{{ url('assets/modules/bootstrap-social/bootstrap-social.css') }}">
+
+    <!-- Template CSS -->
+    <link rel="stylesheet" href="{{ url('assets/css/style.css') }}">
+    <link rel="stylesheet" href="{{ url('assets/css/components.css') }}">
+</head>
+
+<body>
+    <div id="app">
+
+        @guest
+            <div class="container">
+                <section class="section">
+                    <div class="container mt-5">
+                        <div class="row">
+                            <div
+                                class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
+                                <div class="login-brand">
+                                    <img src="{{ url('assets/img/stisla-fill.svg') }}" alt="logo" width="100"
+                                        class="shadow-light rounded-circle">
+                                </div>
+
+                                <div class="card card-primary">
+                                    <div class="card-header">
+                                        <h4>Login</h4>
+                                    </div>
+
+                                    <div class="card-body">
+                                        <form method="POST" action="{{ route('login') }}">
+                                            @csrf
+                                            <div class="form-group">
+                                                <label for="email">Email</label>
+                                                <input id="email" type="email"
+                                                    class="form-control @error('email') is-invalid @enderror" name="email"
+                                                    value="{{ old('email') }}" tabindex="1" required
+                                                    autocomplete="email" autofocus>
+                                                @error('email')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
+                                            </div>
+
+                                            <div class="form-group">
+                                                <div class="d-block">
+                                                    <label for="password" class="control-label">Password</label>
+                                                    @if (Route::has('password.request'))
+                                                        <div class="float-right">
+                                                            <a href="{{ route('password.request') }}" class="text-small">
+                                                                Forgot Password?
+                                                            </a>
+                                                        </div>
+                                                    @endif
+                                                </div>
+                                                <input id="password" type="password"
+                                                    class="form-control @error('password') is-invalid @enderror"
+                                                    name="password" tabindex="2" required autocomplete="current-password">
+                                                @error('password')
+                                                    <span class="invalid-feedback" role="alert">
+                                                        <strong>{{ $message }}</strong>
+                                                    </span>
+                                                @enderror
+                                            </div>
+
+                                            <div class="form-group">
+                                                <div class="custom-control custom-checkbox">
+                                                    <input type="checkbox" name="remember" class="custom-control-input"
+                                                        tabindex="3" id="remember"
+                                                        {{ old('remember') ? 'checked' : '' }}>
+                                                    <label class="custom-control-label" for="remember-me">Remember
+                                                        Me</label>
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <button type="submit" class="btn btn-primary btn-lg btn-block"
+                                                    tabindex="4">
+                                                    Login
+                                                </button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </div>
+                                <div class="mt-5 text-muted text-center">
+                                    Don't have an account? <a href="{{ route('register') }}">Create One</a>
+                                </div>
+                                <div class="simple-footer">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        @else
+            <li class="nav-item dropdown">
+                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                    data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    {{ Auth::user()->name }}
+                </a>
+
+                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </div>
+            </li>
+        @endguest
+
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+
+    <!-- General JS Scripts -->
+    <script src="{{ url('assets/modules/jquery.min.js') }}"></script>
+    <script src="{{ url('assets/modules/popper.js') }}"></script>
+    <script src="{{ url('assets/modules/tooltip.js') }}"></script>
+    <script src="{{ url('assets/modules/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="{{ url('assets/modules/nicescroll/jquery.nicescroll.min.js') }}"></script>
+    <script src="{{ url('assets/modules/moment.min.js') }}"></script>
+    <script src="{{ url('assets/js/stisla.js') }}"></script>
+
+    <!-- JS Libraies -->
+
+    <!-- Page Specific JS File -->
+
+    <!-- Template JS File -->
+    <script src="{{ url('assets/js/scripts.js') }}"></script>
+    <script src="{{ url('assets/js/custom.js') }}"></script>
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+Auth::routes();
+
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/test', function () {
+    return view('auth.logins');
+});
+Route::get('/template', function () {
+    return view('layouts.template');
+});
